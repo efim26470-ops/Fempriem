9f8533970ddd6f52b6f1db1263d47333ed044a44
efim26470-ops <efim26470@gmail.com>
2026-06-18 16:15:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fem-admission-2026-v4';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/apple-touch-icon.png','./icons/favicon-32.png','./icons/icon-192.png','./icons/icon-512.png','./icons/maskable-192.png','./icons/maskable-512.png'];
+const CACHE='fem-admission-2026-v5';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./favicon-32.png','./icon-192.png','./icon-512.png','./maskable-192.png','./maskable-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
